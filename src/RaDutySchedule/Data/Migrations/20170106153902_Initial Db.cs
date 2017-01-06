0b04f2864d802d0f7da89d807212f6b0227da994
Ksandros Apostoli <Ksandros Apostoli>
2017-01-06 17:44:53 +0200

    Migrations for DB

---
+++ src/RaDutySchedule/Data/Migrations/20170106153902_Initial Db.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RaDutySchedule.Data.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Duties",
+                columns: table => new
+                {
+                    DutyID = table.Column<string>(nullable: false),
+                    DutyDate = table.Column<DateTime>(nullable: false),
+                    aubgID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Duties", x => x.DutyID);
+                });
+
+            migrationBuilder.AddColumn<string>(
+                name: "aubgID",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "aubgID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Duties");
+        }
+    }
+}
